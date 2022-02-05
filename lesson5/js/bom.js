b3a7e7c7a80d5e0a4dd72b96a339d3c16e99fc28
jasnatbyui <jasnatbyui@gmail.com>
2022-02-05 22:08:26 +1300

    bom assignment + css Chamber adds

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,8 +1,42 @@
-
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 
+button.onclick = function() {
+let myItem = input.value;
+input.value = '';
+
+const listItem = document.createElement('li');
+const listText = document.createElement('span');
+const listBtn = document.createElement('button');
+
+listItem.appendChild(listText);
+listText.textContent = myItem;
+listItem.appendChild(listBtn);
+listBtn.textContent = '❌';
+list.appendChild(listItem);
+
+listBtn.onclick = function(e) {
+  list.removeChild(listItem);
+}
+
+input.focus();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
   const listChapter = document.createElement('li');
   const inputChapter = document.createElement('span');
@@ -11,7 +45,6 @@ const button = document.querySelector('button');
   listButton.addEventListener('click', function() {  
       let myItem = input.value;
         input.value = '';
-  
 
   listChapter.appendChild(inputChapter);
   inputChapter.textContent = myItem;
@@ -22,6 +55,5 @@ const button = document.querySelector('button');
   listButton.addEventListener('click', function(e) {  
     list.removeChild(listChapter);
   }
-}
-
-  /*input.focus(); */
+  input.focus(); 
+  } */
