79660116a720cc28ecf83ecdd741fe91f8801cd3
jasnatbyui <jasnatbyui@gmail.com>
2022-03-15 22:58:40 +1300

    Resubmit changes

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,28 +1,46 @@
-const list = document.querySelector('ul');
-const input = document.querySelector('input');
-const button = document.querySelector('button');
+// const list = document.querySelector('ul');
+// const input = document.querySelector('input');
+// const button = document.querySelector('button');
 
-button.onclick = function() {
-let myItem = input.value;
-input.value = '';
+// button.onclick = function() {
+// let myItem = input.value;
+// input.value = '';
 
-const listItem = document.createElement('li');
-const listText = document.createElement('span');
-const listBtn = document.createElement('button');
+// const listItem = document.createElement('li');
+// const listText = document.createElement('span');
+// const listBtn = document.createElement('button');
 
-listItem.appendChild(listText);
-listText.textContent = myItem;
-listItem.appendChild(listBtn);
-listBtn.textContent = '❌';
-list.appendChild(listItem);
+// listItem.appendChild(listText);
+// listText.textContent = myItem;
+// listItem.appendChild(listBtn);
+// listBtn.textContent = '❌';
+// list.appendChild(listItem);
 
-listBtn.onclick = function(e) {
-  list.removeChild(listItem);
-}
+// listBtn.onclick = function(e) {
+//   list.removeChild(listItem);
+// }
 
-input.focus();
-}
+// input.focus();
+// }
+const input = document.getElementById("favchap");
+const button = document.querySelector("button");
+const list = document.querySelector(".list");
 
+button.addEventListener('click', function () {
+    if (input.value) {
+        let newListItem = document.createElement('li');
+        let newDeleteButton = document.createElement('button');
+        newListItem.innerHTML = input.value;
+        newDeleteButton.textContent = "❌";
+        newListItem.appendChild(newDeleteButton);
+        list.appendChild(newListItem);
+        newDeleteButton.addEventListener('click', function() {
+            newListItem.remove();
+        });
+        input.value = "";
+        input.focus();
+        }
+    })
 
 
 
