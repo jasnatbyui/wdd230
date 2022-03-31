7a5f5e9a15af984b44fbce99a5c4883f913e3919
jasnatbyui <jasnatbyui@gmail.com>
2022-03-31 22:38:03 +1300

    temples directory

--- Temple-Inn/js/directory.js
+++ Temple-Inn/js/directory.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://jasnatbyui.github.io/wdd230/Chamber/data/data.json'
+const requestURL = 'https://jasnatbyui.github.io/wdd230/Temple-Inn/data/data.json'
 const directories = document.querySelector('.directory');
 
 fetch(requestURL)
@@ -11,26 +11,48 @@ fetch(requestURL)
 })
 .then(function (jsonObject) {
     console.table(jsonObject);
-    const businesses = jsonObject['businesses'];
-    businesses.forEach(dispayBusinesses);
+    const temples = jsonObject['temples'];
+    temples.forEach(dispayTemples);
 });
 
-function dispayBusinesses(business) {
+function dispayTemples(temple) {
     let directory = document.createElement('section');
     let h2Name = document.createElement('h2');
     let h2Address = document.createElement('h2');
     let h2PhNumber = document.createElement('h2');
     let h2Website = document.createElement('h2');
     let h2Email = document.createElement('h2');
+    let ulServices = document.createElement('h2');
+    let tdTimetable = document.createElement('td');
+    let h2Announcement = document.createElement('h2');
+    let h2Dedication = document.createElement('h2');
+    let h2Ordinance = document.createElement('h2');
+    let h2Locale = document.createElement('h2');
+    let pFacts1 = document.createElement('h2');
+    let pFacts2 = document.createElement('h2');
+    let pFacts3 = document.createElement('h2');
+    let pFacts4 = document.createElement('h2');
     let screenshot = document.createElement('img', {className: 'bus-img'});
 
-    h2Name.innerHTML = `<span class="businessname">${business.name}</span>`;
-    h2Address.innerHTML = `<span class="address">${business.address}</span>`;
-    h2PhNumber.innerHTML = `<span class="phonenumber">Phone number: ${business.phonenumber}</span>`;
-    h2Website.innerHTML = `<span class="website">Website: ${business.website}</span>`;
-    h2Email.innerHTML = `<span class="email">Email: ${business.email}</span>`;
-    screenshot.setAttribute('src', business.imageurl);
-    screenshot.setAttribute('alt', `Directory of ${business.name}`);
+    h2Name.innerHTML = `<span class="businessname">${temple.name}</span>`;
+    h2Address.innerHTML = `<span class="address">${temple.address}</span>`;
+    h2PhNumber.innerHTML = `<span class="phonenumber">Phone number: ${temple.phonenumber}</span>`;
+    h2Website.innerHTML = `<span class="website">Website: ${temple.website}</span>`;
+    h2Email.innerHTML = `<span class="email">Email: ${temple.email}</span>`;
+    ulServices.innerHTML = `<span class="services">${temple.services}</span>`;
+    tdTimetable.innerHTML = `<span class="timetable">${temple.timetable}</span>`;
+    h2Announcement.innerHTML = `<span class="announcement">${temple.announcement}</span>`;
+    h2Dedication.innerHTML = `<span class="dedication">${temple.dedication}</span>`;
+    h2Ordinance.innerHTML = `<span class="ordinance">${temple.ordinancerooms}</span>`;
+    h2Locale.innerHTML = `<span class="locale">${temple.locale}</span>`;
+    pFacts1.innerHTML = `<span class="facts1">${temple.factsp1}</span>`;
+    pFacts2.innerHTML = `<span class="facts2">${temple.factsp2}</span>`;
+    pFacts3.innerHTML = `<span class="facts3">${temple.factsp3}</span>`;
+    pFacts4.innerHTML = `<span class="facts4">${temple.factsp4}</span>`;
+
+
+    screenshot.setAttribute('src', temple.imageurl);
+    screenshot.setAttribute('alt', `Directory of ${temple.name}`);
     screenshot.setAttribute('loading', 'lazy');
 
     directory.appendChild(h2Name);
@@ -38,6 +60,18 @@ function dispayBusinesses(business) {
     directory.appendChild(h2PhNumber);
     directory.appendChild(h2Website);
     directory.appendChild(h2Email);
+    directory.appendChild(ulServices);
+    directory.appendChild(tdTimetable);
+    directory.appendChild(h2Announcement);
+    directory.appendChild(h2Dedication);
+    directory.appendChild(h2Ordinance);
+    directory.appendChild(h2Locale);
+    directory.appendChild(pFacts1);
+    directory.appendChild(pFacts2);
+    directory.appendChild(pFacts3);
+    directory.appendChild(pFacts4);
+
+
     directory.appendChild(screenshot);
 
     directories.appendChild(directory);
